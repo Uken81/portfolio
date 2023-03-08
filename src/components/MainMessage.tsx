31cd3bb83cfa67d2dbf82abd2acdcda17dc2cf5c
Brendan Hurd <brendanhurd@gmail.com>
2023-03-08 15:55:55 +1100

    Shrink gradient margin on links.

--- src/components/MainMessage.tsx
+++ src/components/MainMessage.tsx
@@ -21,7 +21,7 @@ export const MainMessage: React.FC<{
   );
 
   const Message = (props: Project) => (
-    <div className="mb-24 flex flex-col sm:px-24 md:px-48 lg:px-60">
+    <div className="mb-24 flex flex-col">
       <div className="mb-6 flex cursor-default justify-center text-center">
         <h1 className="bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text p-1 text-4xl font-extrabold text-transparent hover:bg-gradient-to-l">
           {props.title}
@@ -32,7 +32,7 @@ export const MainMessage: React.FC<{
           <span dangerouslySetInnerHTML={{ __html: props.text }}></span>
         </div>
         {project.links && (
-          <div className="ml-2 flex max-w-lg bg-gray-900 pt-4 pl-4">
+          <div className="ml-1 flex max-w-lg bg-gray-900 pt-4 pl-4">
             <div className="ml-4 bg-gradient-to-r from-sky-500 to-indigo-500 hover:bg-gradient-to-l">
               <Link linkRef={project.links.site} linkText="View Site" />
             </div>
