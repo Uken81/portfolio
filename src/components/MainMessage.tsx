305142cbd009a888ae57719dcde55de220a9518a
Brendan Hurd <brendanhurd@gmail.com>
2023-03-10 19:25:43 +1100

    Create variables for css gradients for readability.

--- src/components/MainMessage.tsx
+++ src/components/MainMessage.tsx
@@ -1,14 +1,18 @@
 import { Project, projects } from './common/projectsList';
 
+const gradientRight =
+  'bg-gradient-to-r from-sky-500 to-indigo-500 hover:bg-gradient-to-l';
+
+const gradientBottom =
+  'bg-gradient-to-b from-sky-500 to-indigo-500 hover:bg-gradient-to-l';
+
 const Titles: React.FC<{ title: string; subtitle?: string }> = ({
   title,
   subtitle
 }) => (
   <div className="mb-6 flex cursor-default flex-col justify-center text-center">
     <h1
-      className={
-        'bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text p-1 text-4xl font-extrabold text-transparent'
-      }>
+      className={`${gradientBottom} bg-clip-text p-1 text-4xl font-extrabold text-transparent`}>
       {title}
     </h1>
     {subtitle && <h2 dangerouslySetInnerHTML={{ __html: subtitle }}></h2>}
@@ -32,16 +36,16 @@ const Message: React.FC<{
   return (
     <div className="mb-24 flex flex-col">
       <Titles title={project.title} subtitle={project.subtitle} />
-      <div className="max-w-lg bg-gradient-to-b from-sky-500 to-indigo-500">
+      <div className={`max-w-lg ${gradientBottom}`}>
         <div className="ml-1 cursor-default bg-gray-900 pl-4 pr-8 text-sm leading-6">
           <span dangerouslySetInnerHTML={{ __html: project.text }}></span>
         </div>
         {project.links && (
           <div className="ml-1 flex max-w-lg bg-gray-900 pt-4 pl-4">
-            <div className="ml-4 bg-gradient-to-r from-sky-500 to-indigo-500 hover:bg-gradient-to-l">
+            <div className={`${gradientRight} hover:bg-gradient-to-l`}>
               <Link linkRef={project.links.site} linkText="View Site" />
             </div>
-            <div className="ml-8 bg-gradient-to-r from-sky-500 to-indigo-500 hover:bg-gradient-to-l">
+            <div className={`ml-8 ${gradientRight} hover:bg-gradient-to-l`}>
               <Link linkRef={project.links.github} linkText="GitHub" />
             </div>
           </div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,11 @@
 import { projects } from './common/projectsList';
 
+const gradientRight =
+  'bg-gradient-to-r from-sky-500 to-indigo-500 hover:bg-gradient-to-l';
+
+const gradientBottom =
+  'bg-gradient-to-b from-sky-500 to-indigo-500 hover:bg-gradient-to-l';
+
 export const Projects: React.FC<{
   setSelectedProject: React.Dispatch<React.SetStateAction<string>>;
   closeDemo: () => void;
@@ -13,7 +19,7 @@ export const Projects: React.FC<{
   };
 
   const Project = (props: { onClickParam: string; title: string }) => (
-    <div className="max-w-fit bg-gradient-to-br from-sky-500 to-indigo-500">
+    <div className={`max-w-fit ${gradientRight}`}>
       <p
         onClick={() => handleClick(props.onClickParam)}
         className="mb-0.5 cursor-pointer whitespace-nowrap bg-gray-900 pt-2 duration-300">
@@ -24,9 +30,10 @@ export const Projects: React.FC<{
 
   return (
     <div className="pb-24">
-      <div className="flex h-10 w-20 rounded-l bg-gradient-to-r from-sky-500 to-indigo-500 pl-1 pt-1">
+      <div className={`flex h-10 w-20 rounded-l ${gradientRight} pl-1 pt-1`}>
         <div className="flex flex-col  bg-gray-900 p-4">
-          <h1 className="mb-2 cursor-default bg-gradient-to-br from-sky-500 to-indigo-500 bg-clip-text text-xl font-extrabold text-transparent">
+          <h1
+            className={`mb-2 cursor-default ${gradientBottom} bg-clip-text text-xl font-extrabold text-transparent`}>
             Projects
           </h1>
           {projectList.map((project) => (
