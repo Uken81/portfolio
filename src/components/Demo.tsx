2ebf879355dedda0805d9cc5a71ec624fe827c68
Brendan Hurd <brendanhurd@gmail.com>
2023-03-15 12:20:48 +1100

    Refactor the Demo component to be consistently padded with the rest of the page. Make the demo gif have a larger height.

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -21,15 +21,17 @@ export const Demo: React.FC<{
   }, [selectedProject]);
 
   return image !== '' ? (
-    <div className="flex flex-col justify-center p-16">
+    <div className="flex flex-col items-center px-6 pb-6">
       {/* <div className="transform transition-transform duration-500 hover:scale-150 "> */}
-      <AiOutlineClose
-        onClick={() => setImage('')}
-        className="ml-auto mr-1 mb-1 cursor-pointer bg-gray-900 hover:text-white"
-      />
-      {/* </div> */}
-      <div className="rounded border-8 border-double border-gray-400">
-        <img src={image} className="max-h-72 " />
+      <div>
+        <AiOutlineClose
+          onClick={() => setImage('')}
+          className="ml-auto mr-1 mb-1 cursor-pointer bg-gray-900 hover:text-white"
+        />
+        <div className="rounded border-8 border-double border-stone-300">
+          <img src={image} className="max-h-96 " />
+          {/* <img src={image} className="max-h-72 " /> */}
+        </div>
       </div>
     </div>
   ) : null;
