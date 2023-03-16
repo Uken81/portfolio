3d48ab14f8f7f9b6ac2840830d887d6a722e19d7
Brendan Hurd <brendanhurd@gmail.com>
2023-03-16 13:41:20 +1100

    Create conditional for adding a margin to the main message if it is the welcome message.

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,13 @@ import './style.css';
 
 function App() {
   const [selectedProject, setSelectedProject] = useState<ProjectName>('welcome');
+  const welcomeMargin = selectedProject === 'welcome' ? 'lg:mt-56' : null;
 
   return (
     <div className="min-h-screen bg-gray-900 p-6  text-stone-300 sm:px-12 md:px-24 lg:px-36">
       <Header setSelectedProject={setSelectedProject} />
       <Demo selectedProject={selectedProject} />
-      <div className="lg:flex xl:px-36 2xl:px-64">
+      <div className={`${welcomeMargin} lg:flex lg:px-10 xl:px-36 2xl:px-64`}>
         <MainMessage selectedProject={selectedProject} />
         <Projects setSelectedProject={setSelectedProject} />
       </div>
